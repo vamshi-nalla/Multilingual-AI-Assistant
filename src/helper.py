93fbe8eedaa16f365a0667d4399c34b5158eb554
Vamshi Nalla <nvkrishna9396@gmail.com>
2024-04-16 12:58:22 +0530

    app.py and helper.py code added

--- src/helper.py
+++ src/helper.py
@@ -3,3 +3,22 @@ import google.generativeai as genai
 from dotenv import load_dotenv
 import os
 from gtts import gTTS
+
+
+load_dotenv()
+
+GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY")
+os.environ["GOOGLE_API_KEY"]=GOOGLE_API_KEY
+
+
+
+def voice_input():
+   pass
+    
+
+def text_to_speech(text):
+    pass
+
+def llm_model_object(user_text):
+    pass
+    
